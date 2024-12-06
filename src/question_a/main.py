bde3bc56b8de2aad17b5df5ea2f2f56c1299d8db
nikkipagdanganan <nikki.pagdanganan@g.austincc.edu>
2024-12-06 22:46:34 +0000

    final exam question 1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,35 @@
-#add import
+import question_a 
+
+def stock_purchase_history():
+    aapl = question_a.Stock('AAPL', 'Apple Inc')
+    cat = question_a.Stock('CAT', 'Caterpillar')
+    ek = question_a.Stock('EK', 'Eastman Kodak')
+    goog = question_a.Stock('GOOG', 'Google')
+    msft = question_a.Stock('MSFT', 'Microsoft')
+    stock_dictionary = {aapl.return_symbol() : aapl.return_company_name(),
+                        cat.return_symbol() : cat.return_company_name(),
+                        ek.return_symbol() : ek.return_company_name(),
+                        goog.return_symbol() : goog.return_company_name(),
+                        msft.return_symbol() : msft.return_company_name()}
+    for symbol, name in stock_dictionary.items():
+        print (f"{symbol} | {name}")
+
+
+def menu():
+    print("""
+1-Display stock purchase history
+2-Exit
+          """)
+    selection = input('Type a selection and press Enter: ')
+    if selection == "1":
+        stock_purchase_history()
+        menu()
+    elif selection == "2":
+        print('Exiting...')
+    else:
+        print('INPUT INVALID. Type a selection from the menu and press Enter:  ')
+        menu()
+
+def main():
+    menu()
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,28 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def return_symbol(self):
+        return self.__symbol
+    
+    def return_company_name(self):
+        return self.__company_name
+    
+    def stock_purchase_history(self):
+        aapl = Stock('AAPL', 'Apple Inc')
+        cat = Stock('CAT', 'Caterpillar')
+        ek = Stock('EK', 'Eastman Kodak')
+        goog = Stock('GOOG', 'Google')
+        msft = Stock('MSFT', 'Microsoft')
+        stock_dictionary = {aapl.return_symbol : aapl.return_company_name,
+                            cat.return_symbol : cat.return_company_name,
+                            ek.return_symbol : ek.return_company_name,
+                            goog.return_symbol : goog.return_company_name,
+                            msft.return_symbol : msft.return_company_name,}
+        for stock in stock_dictionary:
+            print (stock)
