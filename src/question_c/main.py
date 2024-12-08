f33ed6f3b52609219d2043588927cb721aefeffe
nikkipagdanganan <nikki.pagdanganan@g.austincc.edu>
2024-12-08 21:14:50 +0000

    question 3

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,30 @@
 #add import
+import question_c
+
+def menu():
+    menu = int(input("""
+Multiplication Table Menu
+1 - Display Table
+2 - Quit
+"""))
+    return menu
+
+def main():
+    cont = True
+    while True:
+        selection = menu()
+
+        if selection == 1:
+            display_vals = question_c.display_multiplication_table()
+            print(display_vals)
+
+        elif selection == 2:
+            cont = False
+            print('Quitting...')
+            break
+        
+
+        else:
+            print('Invalid input. Select 1 or 2.')
+
+main()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,31 @@
 #write functions here, don't add input('') statements here!
+
+# def create_multiplication_table(rows, cols):
+#     list0 = []
+#     # the list above needs to be populated by 5 lists
+#     for i in range(0, rows):
+#         row_list = []
+#         for j in range(0, cols):
+#             row_list.append((i + 1) * (j + 1))
+#         list0.append(row_list)
+#     print(list0)
+
+# create_multiplication_table(5,10)
+    
+def create_multiplication_table():
+    list0 = []
+    for i in range(5):
+        row_list = []
+        for j in range(10):
+            row_list.append((i + 1) * (j + 1))
+        list0.append(row_list)
+    return list0
+
+def display_multiplication_table():
+    list0 = create_multiplication_table()
+    for row_list in list0:
+        for item in row_list:
+            print(str(item).rjust(3, " "), end = " ")
+
+        print(" ")
+
