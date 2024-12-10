be5f783909434dd003336597b4c2336df2363e50
nikkipagdanganan <nikki.pagdanganan@g.austincc.edu>
2024-12-10 15:17:38 +0000

    question 4

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,29 @@
 #add import
+import question_d
+
+def menu():
+    selection = int( input( """
+Stock List Menu:
+1- Display Stock Report
+2- Exit program
+"""))
+    return selection
+
+def main():
+    while True:
+        choice = menu()
+
+        if choice == 1:
+            list_display = question_d.Stock.get_stock_list()
+            print(f"Stock Purchase history:  \n {list_display}")
+            for stock in list_display:
+                print(stock.get_symbol() + ' ' + stock.get_company_name())
+        elif choice == 2:
+            print('Quitting...')
+            break
+            
+        else:
+            print('Invalid choice. Select 1 or 2 from menu.')
+         
+
+main()
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,36 @@
 #write functions here, don't add input('') statements here!
+class Stock:
+    
+    __symbol = ""
+    __company_name = ""
+    
+    def __init__(self, symbol, company_name):
+
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def get_company_name(self):
+        return self.__company_name
+
+    def get_stock_list():
+        stock_list = []
+
+        stock1 = Stock('AAPL', 'Apple Inc')
+        stock_list.append(stock1)
+
+        stock2 = Stock('CAT', 'Caterpillar')
+        stock_list.append(stock2)
+
+        stock3 = Stock('EK', 'Eastman Kodak')
+        stock_list.append(stock3)
+
+        stock4 = Stock('GOOG', 'Google')
+        stock_list.append(stock4)
+
+        stock5 = Stock('MSFT', 'Microsoft')
+        stock_list.append(stock5)
+
+        return stock_list
